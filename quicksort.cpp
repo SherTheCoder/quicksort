a025e904ee45a5a45e65c28928ffb471d3d96351
SherTheCoder <43815133+SherTheCoder@users.noreply.github.com>
2025-02-19 12:23:42 +0530

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -4,23 +4,23 @@ public:
         quickSort(nums, 0, nums.size() - 1);
         return nums;
     }
-        void quickSort(vector<int>& nums, int p, int r){
-        if(p<r){
-            int q = partition(nums, p , r);
-            quickSort(nums, p, q-1);
-            quickSort(nums, q+1, r);
+        void quickSort(vector<int>& nums, int low, int high){
+        if(low < high){
+            int pivotIndex = partition(nums, low ,  high);
+            quickSort(nums, low, pivotIndex - 1);
+            quickSort(nums, pivotIndex + 1, high);
         }
     }
-    int partition(vector<int>& nums, int p, int r){
-        int x = nums[r];
-        int i = p - 1;
-        for(int j  = p ; j <= r - 1; j++){
-            if(nums[j] <= x){
+    int partition(vector<int>& nums, int low, int high){
+        int pivotElement = nums[high];
+        int i = low - 1;
+        for(int j  = low ; j <= high - 1; j++){
+            if(nums[j] <= pivotElement){
                 i = i+1;
                 swap(nums[j], nums[i]);
             }
         }
-        swap(nums[i+1], nums[r]);
+        swap(nums[i+1], nums[high]);
         return i+1;
     }
-}
+};
